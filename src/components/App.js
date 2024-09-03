5814a92983d1a193712eac713d1dbc0adce4b856
Shubham Kashyap <shubhamkashyap107@gmail.com>
2024-09-03 21:34:41 +0530

    Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,61 +1,50 @@
-
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
-
-  const[data,setData] = useState([])
-  const[loading,setLoading] = useState(false)
-
-
-  // function btnClickHandler(){
-  //   const getData = async() => {
-  //     const rawData = await fetch("https://reqres.in/api/users")
-  //     const json = await rawData.json()
-  //     setData(json.data)
-  //   }
-  // }
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   function btnClickHandler() {
-    setLoading(!loading)
-    const getData = () => {
-      fetch("https://reqres.in/api/users")
-        .then(rawData => rawData.json())
-        .then(json => {
-          setData(json.data);
-          console.log(json.data)
-          setLoading(false)
-        })
-        .catch(error => {
-          console.error('Error fetching data:', error);
-        });
-    };
-    getData()
+    setLoading(true); // Start loading
+    fetch("https://reqres.in/api/users")
+      .then(rawData => rawData.json())
+      .then(json => {
+        setData(json.data);
+        setLoading(false); // Stop loading
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+        setLoading(false); // Stop loading in case of error
+      });
   }
-  
 
   return (
     <div>
-        
-        <button className="btn" onClick={btnClickHandler}>{loading ? "Loading..." : "Get User List"}</button>
-
+      <button className="btn" onClick={btnClickHandler}>
+        {loading ? "Loading..." : "Get User List"}
+      </button>
 
       <table>
-
-        {data.length ? data.map((item) => {
-          return <tr>
-            <td>{item.first_name}</td>
-            <td>{item.last_name}</td>
-            <td>{item.email}</td>
-            <td><img src={item.avatar} /></td>
+        <tbody>
+          {data.length ? (
+            data.map((item, index) => (
+              <tr key={index}>
+                <td>{item.first_name}</td>
+                <td>{item.last_name}</td>
+                <td>{item.email}</td>
+                <td><img src={item.avatar} alt="User Avatar" /></td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="4">No data found to display</td>
             </tr>
-        }): <td>No data found to display</td>}
-
-    
-        </table>
-
+          )}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
