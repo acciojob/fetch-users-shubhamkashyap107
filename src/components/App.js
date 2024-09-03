8797796b186bb9de91d8dfc62e833e8ff5718a04
Shubham Kashyap <shubhamkashyap107@gmail.com>
2024-09-03 21:21:15 +0530

    Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,58 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const[data,setData] = useState([])
+  const[loading,setLoading] = useState(false)
+
+
+  // function btnClickHandler(){
+  //   const getData = async() => {
+  //     const rawData = await fetch("https://reqres.in/api/users")
+  //     const json = await rawData.json()
+  //     setData(json.data)
+  //   }
+  // }
+
+  function btnClickHandler() {
+    setLoading(!loading)
+    const getData = () => {
+      fetch("https://reqres.in/api/users")
+        .then(rawData => rawData.json())
+        .then(json => {
+          setData(json.data);
+          console.log(json.data)
+          setLoading(false)
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
+        });
+    };
+    getData()
+  }
+  
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        
+        <button onClick={btnClickHandler}>{loading ? "Loading..." : "Get User List"}</button>
+
+
+      <table>
+
+        {data.map((item) => {
+          return <tr>
+            <td>{item.first_name}</td>
+            <td>{item.last_name}</td>
+            <td>{item.email}</td>
+            <td><img src={item.avatar} /></td>
+            </tr>
+        })}
+
+        </table>
+
     </div>
   )
 }
